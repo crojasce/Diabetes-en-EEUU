65ce6a2daafbd849ffec72d84238402dc1393e94
crojasce <crojasce@unbosque.edu.co>
2025-08-13 16:03:45 -0500

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,9 +1,16 @@
 
 # (Opcional) crear y activar un entorno virtual
-python -m venv .venv
-Windows: .venv\Scripts\activate
-macOS/Linux: source .venv/bin/activate
+# python -m venv .venv
+# Windows: .venv\Scripts\activate
+# macOS/Linux: source .venv/bin/activate
+# venv
+source .venv/bin/activate      # Linux / Mac
+.venv\Scripts\activate         # Windows
+
+# conda
+conda activate mi_entorno
+
+pip install prince
 
 pip install -r requirements.txt
 streamlit run app.py
-pip install prince
