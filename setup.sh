f746d6d3288c0d3a7fe3f90dbca249f5889dd4d9
crojasce <crojasce@unbosque.edu.co>
2025-08-13 16:04:14 -0500

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -3,14 +3,6 @@
 # python -m venv .venv
 # Windows: .venv\Scripts\activate
 # macOS/Linux: source .venv/bin/activate
-# venv
-source .venv/bin/activate      # Linux / Mac
-.venv\Scripts\activate         # Windows
 
-# conda
-conda activate mi_entorno
 
 pip install prince
-
-pip install -r requirements.txt
-streamlit run app.py
